b8b8b420fccf225f2fd33b5af5b5d5cf24860c22
JackTench <79285604+JackTench@users.noreply.github.com>
2026-08-19 22:06:48 +0100

    Create structs for LibreSplit file types

---
+++ src/libresplit/lssplit.rs
@@ -0,0 +1,7 @@
+pub struct LibreSplitSplit {
+    pub title: String,
+    pub icon: String,
+    pub time: String,
+    pub best_time: String,
+    pub best_segment: String,
+}
---
+++ src/libresplit/lssplitfile.rs
@@ -0,0 +1,14 @@
+use crate::libresplit::lssplit::LibreSplitSplit;
+
+pub struct LibreSplitSplitFile {
+    pub title: String,
+    pub attempt_count: i32,
+    pub comparison_method: i32,
+    pub start_delay: String,
+    pub world_record: String,
+    pub splits: Vec<LibreSplitSplit>,
+    pub theme: String,
+    pub theme_variant: String,
+    pub width: i32,
+    pub height: i32,
+}

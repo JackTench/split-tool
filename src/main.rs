7711f3fa29a0245f60d8c0be9f38ba548e350777
JackTench <79285604+JackTench@users.noreply.github.com>
2026-08-20 21:46:02 +0100

    Test conversion in main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
+use std::{fs::read_to_string, io::Cursor};
+
+use spex::parsing::XmlReader;
+
+use crate::converter::{ConvertableSplitFile, livesplit::livesplitsplitfile::LiveSplitSplitFile};
+
 mod converter;
 mod libresplit;
 
 fn main() {
-    println!("Hello, world!");
+    // Convert test LiveSplit file.
+    let file = read_to_string("test.lss").unwrap();
+    let cursor = Cursor::new(file);
+    let xml = XmlReader::parse_auto(cursor)
+        .map_err(|e| e.to_string())
+        .unwrap();
+    let livesplit = LiveSplitSplitFile::new(xml);
+    let libresplit = livesplit.convert();
+
+    println!("{:?}", libresplit);
 }

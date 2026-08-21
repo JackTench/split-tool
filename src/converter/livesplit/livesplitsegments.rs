a684b98916e07bb8bfc89d9dfd6af178d8f7d544
JackTench <79285604+JackTench@users.noreply.github.com>
2026-08-21 12:55:54 +0100

    Lay out XML structure as rust structs

--- src/converter/livesplit/livesplitsegments.rs
+++ src/converter/livesplit/livesplitsegments.rs
@@ -1,1 +1,21 @@
+pub struct Segments {
+    pub segment: Vec<Segment>,
+}
 
+pub struct Segment {
+    pub name: String,
+    pub split_times: SplitTimes,
+    pub best_segment_time: Time,
+}
+
+pub struct SplitTimes {
+    pub split_time: Vec<SplitTime>,
+}
+
+pub struct SplitTime {
+    pub real_time: String,
+}
+
+pub struct Time {
+    real_time: String,
+}
--- src/converter/livesplit/livesplitsplitfile.rs
+++ src/converter/livesplit/livesplitsplitfile.rs
@@ -1,1 +1,9 @@
+use crate::converter::livesplit::livesplitsegments::Segments;
 
+pub struct LiveSplitRun {
+    pub game_name: String,
+    pub category_name: String,
+    pub platform: String,
+    pub attempt_count: i32,
+    pub segments: Segments,
+}

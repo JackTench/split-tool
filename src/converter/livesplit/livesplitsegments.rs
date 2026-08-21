973caa050fb957d480f9fb87e1c591271b08c21c
JackTench <79285604+JackTench@users.noreply.github.com>
2026-08-21 13:01:07 +0100

    Add serde derives for LiveSplit types

--- src/converter/livesplit/livesplitsegments.rs
+++ src/converter/livesplit/livesplitsegments.rs
@@ -1,21 +1,37 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Default)]
 pub struct Segments {
+    #[serde(rename = "Segment", default)]
     pub segment: Vec<Segment>,
 }
 
+#[derive(Debug, Deserialize, Default)]
 pub struct Segment {
+    #[serde(rename = "Name", default)]
     pub name: String,
+
+    #[serde(rename = "SplitTimes", default)]
     pub split_times: SplitTimes,
+
+    #[serde(rename = "BestSegmentTime", default)]
     pub best_segment_time: Time,
 }
 
+#[derive(Debug, Deserialize, Default)]
 pub struct SplitTimes {
+    #[serde(rename = "SplitTime", default)]
     pub split_time: Vec<SplitTime>,
 }
 
+#[derive(Debug, Deserialize, Default)]
 pub struct SplitTime {
+    #[serde(rename = "RealTime", default)]
     pub real_time: String,
 }
 
+#[derive(Debug, Deserialize, Default)]
 pub struct Time {
+    #[serde(rename = "RealTime", default)]
     real_time: String,
 }
--- src/converter/livesplit/livesplitsplitfile.rs
+++ src/converter/livesplit/livesplitsplitfile.rs
@@ -1,9 +1,22 @@
+use serde::Deserialize;
+
 use crate::converter::livesplit::livesplitsegments::Segments;
 
+#[derive(Debug, Deserialize)]
+#[serde(rename = "Run")]
 pub struct LiveSplitRun {
+    #[serde(rename = "GameName", default)]
     pub game_name: String,
+
+    #[serde(rename = "CategoryName", default)]
     pub category_name: String,
+
+    #[serde(rename = "Platform", default)]
     pub platform: String,
+
+    #[serde(rename = "AttemptCount", default)]
     pub attempt_count: i32,
+
+    #[serde(rename = "Segments", default)]
     pub segments: Segments,
 }

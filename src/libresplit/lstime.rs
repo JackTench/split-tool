41bc5af3e1b347cf7c4999151553cab03ad3a1dd
JackTench <79285604+JackTench@users.noreply.github.com>
2026-09-06 19:11:31 +0100

    Default times to 0.000000

--- src/libresplit/lstime.rs
+++ src/libresplit/lstime.rs
@@ -5,3 +5,12 @@ pub struct LibreSplitTime {
     pub real_time: String,
     pub game_time: String,
 }
+
+impl Default for LibreSplitTime {
+    fn default() -> Self {
+        Self {
+            real_time: "0.000000".to_string(),
+            game_time: "0.000000".to_string(),
+        }
+    }
+}
